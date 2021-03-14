be3044c8b74b03cc8c6b475a245dc294c7720998
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-03-14 10:09:44 -0400

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(driver, string) {
+    let name = driver.filter(n => n.toUpperCase() === string.toUpperCase());
+    return name;
+}
+
+function fuzzyMatch(driver, string) {
+    let name = driver.filter(n => n.substring(0,2) === string.substring(0,2));
+    return name;
+}
+
+function matchName(driver, string) {
+    let name = driver.filter(n => n.name === string);
+    return name;
+}
